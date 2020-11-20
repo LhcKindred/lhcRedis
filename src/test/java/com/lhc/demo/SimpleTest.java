1d60bb44fbf505ebb0ba578a7386699979f91465
LhcKindred <LhcKindred@163.com>
2020-11-20 13:58:03 +0800

    ei

---
+++ src/test/java/com/lhc/demo/SimpleTest.java
@@ -0,0 +1,19 @@
+package com.lhc.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * 菜
+ *
+ * @author liuhongcheng
+ * @since 2020/11/16 10:48
+ */
+@Slf4j
+public class SimpleTest {
+
+    @Test
+    public void easyTest() {
+        log.info("起飞，诶~飞");
+    }
+}
